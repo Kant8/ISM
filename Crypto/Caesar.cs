7627b6f5b1e06d43261e960b5a051f83ca32bfbe
Andrey Kontorovich <darkdancer8@gmail.com>
2014-09-07 12:40:01 +0300

    First commit Added Caesar algorithm

---
+++ Crypto/Caesar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto
+{
+    public class Caesar : ICryptoCoder
+    {
+        public string Encode(string message, object key)
+        {
+            var chars = message.ToCharArray();
+            var shift = (int)key;
+
+            var encodedMessage = new char[chars.Length];
+            for (int i = 0; i < chars.Length; i++)
+            {
+                var encodedC = chars[i] + shift;
+                encodedMessage[i] = (char)encodedC;
+            }
+            return encodedMessage.ToCharString();
+        }
+
+        public string Decode(string message, object key)
+        {
+            var chars = message.ToCharArray();
+            var shift = (int)key;
+
+            var decodedMessage = new char[chars.Length];
+            for (int i = 0; i < chars.Length; i++)
+            {
+                var decodedC = chars[i] - shift;
+                decodedMessage[i] = (char)decodedC;
+            }
+            return decodedMessage.ToCharString();
+        }
+    }
+}
---
+++ Crypto/ICryptoCoder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto
+{
+    public interface ICryptoCoder
+    {
+        string Encode(string message, object key);
+
+        string Decode(string message, object key);
+    }
+}
